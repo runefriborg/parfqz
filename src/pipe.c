980843f6be9d7f237cca4ca3d9bb9af346188262
Anders Egerup Halager <aeh@birc.au.dk>
2014-10-20 13:21:08 +0200

    Whitespace fix

--- src/pipe.c
+++ src/pipe.c
@@ -6,9 +6,9 @@ void pipe_init(pipe_t *pipe)
     memset(pipe->buffer, 0, OUTPUT_BUFFER_SIZE*sizeof(void*));
     pipe->begin = 0;
     pipe->end = 0;
-	pthread_cond_init(&pipe->cond_put, NULL);
-	pthread_cond_init(&pipe->cond_took, NULL);
-	pthread_mutex_init(&pipe->mutex, NULL);
+    pthread_cond_init(&pipe->cond_put, NULL);
+    pthread_cond_init(&pipe->cond_took, NULL);
+    pthread_mutex_init(&pipe->mutex, NULL);
 }
 
 void pipe_put(pipe_t *pipe, void *data)
@@ -33,15 +33,15 @@ void pipe_put(pipe_t *pipe, void *data)
 
 void *pipe_get(pipe_t *pipe)
 {
-	pthread_mutex_lock(&pipe->mutex);
-	while (pipe->begin == pipe->end) {
-		pthread_cond_wait(&pipe->cond_put, &pipe->mutex);
-	}
-	void *data = pipe->buffer[pipe->begin];
+    pthread_mutex_lock(&pipe->mutex);
+    while (pipe->begin == pipe->end) {
+        pthread_cond_wait(&pipe->cond_put, &pipe->mutex);
+    }
+    void *data = pipe->buffer[pipe->begin];
     if (data != NULL)
         pipe->begin = (pipe->begin + 1) % OUTPUT_BUFFER_SIZE;
     pthread_cond_signal(&pipe->cond_took);	
-	pthread_mutex_unlock(&pipe->mutex);
+    pthread_mutex_unlock(&pipe->mutex);
     return data;
 }
 
