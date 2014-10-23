656c8eb7bbb80958956646272a7d7b7ef3a45b88
runefriborg <rune.m.friborg@gmail.com>
2014-10-23 08:27:41 +0200

    Moved chunk_t to chunk.h

--- src/splitstream.h
+++ src/splitstream.h
@@ -5,33 +5,8 @@
 #include <pthread.h>
 
 #include "main.h"
+#include "chunk.h"
 #include "pipe.h"
-
-
-typedef struct {
-  // Chunk maintenaince variables
-  int order; // Used to handle out-of-order execution
-  int read_len;
-  int read_count; // number of reads in chunk
-  
-  // splitstream data  
-  int read_id_offset [COMPRESSION_CHUNK_SIZE];
-  char *read_id_content;                         // read_id_content+read_id_offset[offset]
-  char *read_base;                               // read_base+read_len*offset
-  int read_plus_offset [COMPRESSION_CHUNK_SIZE];
-  char *read_plus_content;                       // read_plus_content+read_plus_offset[offset]
-  char *read_qual;                               // read_qual+read_len*offset
-  
-  // splitchunk data
-  char ** base_len_10;
-  int base_len_10_count;
-  char ** qual_len_10;
-  int qual_len_10_count;
-  //char *base_len_20;
-  //char *base_len_skew; // handle the possibly 0 to 9 bases in the end.
-  //int base_len_skew_len;
-
-} chunk_t;
 
 
 typedef struct {
